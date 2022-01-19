2fe2ee1e097b3a00c19f7934bafaed387589a424
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-19 01:05:25 +0000

    + Overview

--- App.js
+++ App.js
@@ -2,21 +2,32 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Welcome from "./screens/Welcome";
 import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import PayoutItem from "./components/PayoutItem";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar translucent style="auto" />
-      <Welcome />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/DMSans-Regular.ttf"),
+    bold: require("./assets/fonts/DMSans-Bold.ttf"),
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar translucent style="auto" />
+        {/* <Welcome /> */}
+        <PayoutItem />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ecf0f7",
     paddingTop: 30,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
 });
