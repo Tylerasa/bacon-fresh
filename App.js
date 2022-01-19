8daa5d91bb1919216823d34880b7fac06370d49f
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-19 00:34:01 +0000

    +DMSans font

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Welcome from "./screens/Welcome";
+import { useFonts } from "expo-font";
+
 export default function App() {
   return (
     <View style={styles.container}>
